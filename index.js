18ea435980c4842ff59eb7d6fb829787f7cb1665
Rotich <jane.rotich@student.moringaschool.com>
2024-01-12 15:35:32 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  for(let startIndex = 0; startIndex < word.length / 2; startIndex++){
+    const endIndex = word.length - 1 - startIndex;
+    if(word[startIndex] !== word[endIndex]){
+      return false;
+    }
+  }
+  return true
 }
 
 /* 
   Add your pseudocode here
 */
+// iterate from the beginning of the string to the middle of the string
+  // compare the letter we're iterating over to the corresponding letter at the end of the string
+  // if the letters don't match, return false
 
+// if we reach the middle, and all the letters match, return true
 /*
   Add written explanation of your solution here
 */
-
+// Performance: this code is more efficient with regards to time and space complexity, since it creates fewer new variables and only iterates through half of the string
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
